0f6ff2aedb05d58e4342bb9a07cb9aa166b4f722
gonzaga95 <gonzagas.dudu@gmail.com>
2024-07-10 16:04:33 -0400

    add class Compra e metodos de compra na CartaoDeCredito

--- src/carrinhodecompras/modelos/Compra.java
+++ src/carrinhodecompras/modelos/Compra.java
@@ -1,4 +1,24 @@
 package carrinhodecompras.modelos;
 
 public class Compra {
+    private String produto;
+    private double valor;
+
+    public Compra(String produto, double valor) {
+        this.produto = produto;
+        this.valor = valor;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: \nProduto: " + produto + "\nValor: " + valor;
+    }
 }
